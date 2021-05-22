187ef9b19cbccc2f6e825754001668c904e70a02
Archit Aggarwal <archit.aggarwal023@gmail.com>
2021-05-22 12:59:41 +0530

    Update Reverse Stack.cpp

--- Reverse Stack.cpp
+++ Reverse Stack.cpp
@@ -1,34 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-stack<int> Reverse(stack<int> input,stack<int> output)
-{
-    if(input.empty())
-        return output;
-    output.push(input.top());
-    input.pop();
-    return Reverse(input,output);
-}
+
 int main()
 {
     stack<int> stk;
     for(int i=1;i<=5;i++)
      stk.push(i);
+    
+    // Method 1 : Using Another Stack 
+    // O(N) Time Complexity, O(N) Extra Space
+    
     stack<int> inv;
-    // Method 1 : Using Another Stack : O(N) Extra Space
-    // O(N) Time Complexity
     while(!stk.empty())
     {
         inv.push(stk.top());
         stk.pop();
     }
+    
     stk = inv; 
-    // Method 2 : Using Recursion : O(N) Time Complexity
-    stk = Reverse(stk,inv);
+    // Copies the content from Inv stack back into originial stack - O(N) Time 
+    
     while(!stk.empty())
     {
-     cout<<stk.top()<<" ";
-     stk.pop();
+        cout << stk.top() << " ";
+        stk.pop();
     }
-    cout<<endl;
+    
     return 0;
 }
